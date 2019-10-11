8a4b4f6b485c82bc94de337244cf4c2c29cab54f
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 12:25:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,5 +17,11 @@ end
 def sort_array_char_count(array)
   array.sort do |a, b|
     if a.size == b.size
+      0
+    elsif a.size > b.size
+      1
+    elsif a.size < b.size
+      -1
+    end
   end
 end
