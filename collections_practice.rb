b7fd0643d0ebf981753976025bacfd6a8132ffc2
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 12:45:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,65 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.size == b.size
+      0
+    elsif a.size > b.size
+      1
+    elsif a.size < b.size
+      -1
+    end
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |word|
+    word[2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.reduce(0) do |total, current|
+    total += current
+  end
+end
+
+def add_s(array)
+  array.map.with_index do |word, i|
+    if i == 1
+      word
+    else
+      word += "s"
+    end
+  end
+end
